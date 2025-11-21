dadb80670dd0754496fae1891d48ce2ce07f9a09
CatDev3 <168304418+omar3elbakery@users.noreply.github.com>
2025-11-21 23:01:57 +0200

    Replace omni with twrp in AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_gta4lve.mk
+    $(LOCAL_DIR)/twrp_gta4lve.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_gta4lve-user \
-    omni_gta4lve-userdebug \
-    omni_gta4lve-eng
+    twrp_gta4lve-user \
+    twrp_gta4lve-userdebug \
+    twrp_gta4lve-eng
